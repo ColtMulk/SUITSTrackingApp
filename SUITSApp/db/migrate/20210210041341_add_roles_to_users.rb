fed0b45906a63edc3311fd734f26425c7517745d
cfginn <caleb.ginn@gmail.com>
2021-02-09 23:32:27 -0600

    Deleted original user scaffold, created user devise, added rails_admin and cancancan
    You will have to delete suitsappdb and SUITSApp_Test, then create each table again using psql, not rails db:setup, then run rake db:migration

---
+++ SUITSApp/db/migrate/20210210041341_add_roles_to_users.rb
@@ -0,0 +1,12 @@
+class AddRolesToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :role, :integer
+    
+    # Initialize master account:
+    User.create! do |u|
+      u.email = 'master@master.com'
+      u.password = 'password'
+      u.role = 0
+    end
+  end
+end

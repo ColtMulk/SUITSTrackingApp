710d2e3b162d81d9c8ba5c2939f3be0d1a7b41e7
Petitto <gerardo@tamu.edu>
2021-02-17 14:29:00 -0600

    New user controller, user view, home page template

--- SUITSApp/config/routes.rb
+++ SUITSApp/config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   devise_for :users
   
   root 'home#index'
+
+  # resources :dashboard
+  # authenticated :user do
+  #   root :to => "dashboard#index"
+  # end
+  # root :to => redirect("/home/index")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

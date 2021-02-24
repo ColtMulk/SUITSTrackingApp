be35f4b58fa75a4675698e70c138d0903f80280d
coltmulk <coltmulk@gmail.com>
2021-02-24 15:58:01 -0600

    fixed user model merge bug

--- SUITSApp/app/models/user.rb
+++ SUITSApp/app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   enum role: [ :master, :admin, :gen_member ]
+  has_one :user_info, inverse_of: :user, dependent: :destroy, autosave: true
+
+  accepts_nested_attributes_for :user_info
   has_many :attendances, dependent: :delete_all, foreign_key: "users_id"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :user_info
 end

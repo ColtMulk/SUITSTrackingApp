5816f39f48d2b9f146a15bc7914beec61386b912
cameronmac21 <cameronmac99@gmail.com>
2021-03-11 21:37:07 -0600

    adding more rspec testing and brakeman

--- SUITSApp/app/models/event.rb
+++ SUITSApp/app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   has_many :attendances, dependent: :delete_all, foreign_key: "events_id"
-
+  validates_presence_of :event_name, :location, :date, :event_descripition, :passcode
   #belongs_to :event_types
 end
--- SUITSApp/spec/feature/sign_in_test.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-  RSpec.describe 'Verifying user sign in page', type: :feature do
-      scenario 'for new user' do
-        visit new_user_session_path
-        expect(page).to have_content('Log in')
-      end
-    end

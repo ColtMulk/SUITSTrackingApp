163dc4b24db88192c51c18ee8f3d7f65807d6876
Petitto <gerardo@tamu.edu>
2021-03-22 21:12:16 -0500

    Changes to styling of each page. Made sure it is mobile friendly.

--- SUITSApp/app/controllers/user_infos_controller.rb
+++ SUITSApp/app/controllers/user_infos_controller.rb
@@ -1,4 +1,6 @@
 class UserInfosController < ApplicationController
+  layout 'dashboard'
+  
   before_action :set_user_info, only: %i[show edit update destroy]
 
   # GET /user_infos or /user_infos.json

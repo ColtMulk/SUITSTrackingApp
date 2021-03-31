734d9e3e1cf713f6cff9f8b87b8d95f815c0a0c1
cfginn <caleb.ginn@gmail.com>
2021-03-31 15:36:04 -0500

    Created default_for field for member_categories, allows only one category to be default

--- SUITSApp/db/schema.rb
+++ SUITSApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_175311) do
+ActiveRecord::Schema.define(version: 2021_03_31_190806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_175311) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "default_for", default: 0
   end
 
   create_table "user_infos", force: :cascade do |t|
---
+++ SUITSApp/spec/controllers/member_categories_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe MemberCategoriesController, :type => :controller do
+
+  it "should only let one category be default for new users" do
+    
+  end
+
+  it "should only let one category be default for old users" do
+    
+  end
+
+end

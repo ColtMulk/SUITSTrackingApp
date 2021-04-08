7b86232828a3e09f6282866fe8214a72c18e2a4c
cameronmac21 <cameronmac99@gmail.com>
2021-04-08 18:38:39 -0500

    model fix so we can create events

--- SUITSApp/app/models/event.rb
+++ SUITSApp/app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :attendances, dependent: :delete_all, foreign_key: 'events_id', inverse_of: :events
   validates :event_name, :location, :date, :event_description, :passcode_hash,
             :passcode_salt, presence: true
-  belongs_to :event_type, foreign_key: true, inverse_of: :events
+  belongs_to :event_type, foreign_key: 'event_type_id', inverse_of: :events
 
   attr_accessor :passcode
 

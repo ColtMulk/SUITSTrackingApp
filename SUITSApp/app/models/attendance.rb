9e5b6fcabee43f4dadc64af88f47e3c87b333f42
coltmulk <coltmulk@gmail.com>
2021-04-11 17:16:29 -0500

    EventsHelpView

--- SUITSApp/app/models/attendance.rb
+++ SUITSApp/app/models/attendance.rb
@@ -4,6 +4,7 @@ class Attendance < ApplicationRecord
   belongs_to :events, foreign_key: 'events_id', class_name: 'Event', inverse_of: :attendances
   belongs_to :user, foreign_key: 'users_id', inverse_of: :attendances # , foreign_key: 'users_id'
   has_one :user_info, through: :user
+  has_one :event_type, :through => :events 
 
   attr_accessor :user_passcode, :events_passcode_hash
 

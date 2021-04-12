5f6461bbca6077fc7b5a4e1317940db2d0fe8c34
cfginn <caleb.ginn@gmail.com>
2021-04-11 19:53:03 -0500

    Implement member_category specs

--- SUITSApp/spec/views/member_category/index.html.erb_spec.rb
+++ SUITSApp/spec/views/member_category/index.html.erb_spec.rb
@@ -2,6 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe 'member_category/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'member_categories/index.html.erb', type: :view do
+  before do
+    assign(:member_categories, [
+             MemberCategory.create(name: 'test_member_category0', default_for: 0),
+             MemberCategory.create(name: 'test_member_category1', default_for: 0)
+           ])
+  end
+
+  it 'renders a list of member_categories' do
+    render
+  end
 end

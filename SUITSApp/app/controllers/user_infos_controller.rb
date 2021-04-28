2bfba6f3b8d02f543148d54caa152a79b3ce5e93
Clara <cfs743@tamu.edu>
2021-04-27 21:40:25 -0500

    fixing graph issues

--- SUITSApp/app/controllers/user_infos_controller.rb
+++ SUITSApp/app/controllers/user_infos_controller.rb
@@ -13,7 +13,7 @@ class UserInfosController < ApplicationController
   # GET /user_infos/1 or /user_infos/1.json
   def show
     
-    if current_user.user_info.gen_member?
+    if current_user.user_info.gen_member? && current_user.user_info.id != params[:id].to_i
       redirect_to(controller: 'user_infos', action: 'show', id: current_user) 
     end
     @user_info = UserInfo.find_by(user: params[:id])
